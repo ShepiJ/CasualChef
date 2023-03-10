e697e660078f55787784103bcba4b4b8ef2a58bf
ShepiJ <joseenriquesanchishueso@gmail.com>
2023-03-10 21:09:19 +0100

    CasualChef_7
    He metido una forma de poder autenticarse en la base de datos, si existe el usuario o la contraseña no concuerda con el usuario no dejará acceder a la aplicacion.
    
    El xml de login está un poco mal ya que no puedo poner cosas dentro de forma manual pero eso ya lo arreglaré.
    
    Para acabar el login tengo que poner 2 botones:
    
    Recordar sesión: Iniciar con la ultima sesion sin tener que poner los datos otra vez.
    Iniciar sesión como usuario anonimo: Talvez no quieres añadir nada y quieres ver cosas pero será complejo ya que tendré que bloquear las funciones de crear recetas.

--- CasualChef/app/src/main/java/com/jose_sanchis_hueso/CasualChef/Login.kt
+++ CasualChef/app/src/main/java/com/jose_sanchis_hueso/CasualChef/Login.kt
@@ -22,6 +22,7 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthUserCollisionException
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.gson.GsonBuilder
 import com.jose_sanchis_hueso.CasualChef.databinding.ActivityLoginBinding
@@ -30,6 +31,7 @@ import java.io.OutputStream
 class Login : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
+    private lateinit var mAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,11 +39,13 @@ class Login : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        mAuth = FirebaseAuth.getInstance()
+
         val screenSplash = installSplashScreen()
-        screenSplash.setKeepOnScreenCondition{false}
+        screenSplash.setKeepOnScreenCondition { false }
 
         Thread.sleep(1000)
-        screenSplash.setOnExitAnimationListener{splashScreenView ->
+        screenSplash.setOnExitAnimationListener { splashScreenView ->
             val slideBack = ObjectAnimator.ofFloat(
                 splashScreenView.view,
                 View.TRANSLATION_Y,
@@ -54,9 +58,9 @@ class Login : AppCompatActivity() {
                 doOnEnd { splashScreenView.remove() }
             }
 
-            val icon=splashScreenView.iconView
+            val icon = splashScreenView.iconView
             val iconAnimator = ValueAnimator
-                .ofInt(icon.height,0)
+                .ofInt(icon.height, 0)
                 .setDuration(1000)
 
             iconAnimator.addUpdateListener {
@@ -64,7 +68,7 @@ class Login : AppCompatActivity() {
                 icon.layoutParams.width = value
                 icon.layoutParams.height = value
                 icon.requestLayout()
-                if (value==0) slideBack.start()
+                if (value == 0) slideBack.start()
             }
 
             AnimatorSet().apply {
@@ -81,42 +85,90 @@ class Login : AppCompatActivity() {
                 saveFirestoreDataToJson(this)
             }
 
+//El usuario no puede poner un nombre con espacios.
         binding.pasarMain.setOnClickListener {
-            //Guardo las preferencias aqui
+            val email = binding.cogerUsuario.text.toString()+"@gmail.com"
+            val password = binding.cogerContraseA.text.toString()
 
-            var usuarioPreferencia =
-                this?.getSharedPreferences(
-                    "login",
-                    MODE_PRIVATE
-                )
+            //Paso 1. Se intenta hacer el login normal
+            mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        var usuarioPreferencia =
+                            this?.getSharedPreferences(
+                                "login",
+                                MODE_PRIVATE
+                            )
 
-            usuarioPreferencia?.edit()?.putString("username", binding.cogerUsuario.text.toString())?.apply()
+                        usuarioPreferencia?.edit()
+                            ?.putString("username", binding.cogerUsuario.text.toString())?.apply()
 
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        //Paso 2. Se intenta crear una cuenta nueva ya que si el login da error es que no existe o está mal hecha
+                        Log.e(TAG, "signInWithEmailAndPassword failed", task.exception)
+                        mAuth.createUserWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(this) { task ->
+                                if (task.isSuccessful) {
+                                    //Paso 3. Si no hay problema al crear la nueva cuenta se hace login a ella
+                                    //Si no permite crearla es porque ya existe esa cuenta
+                                    mAuth.signInWithEmailAndPassword(email, password)
+                                        .addOnCompleteListener(this) { task ->
+                                            if (task.isSuccessful) {
+                                                var usuarioPreferencia =
+                                                    this?.getSharedPreferences(
+                                                        "login",
+                                                        MODE_PRIVATE
+                                                    )
+                                                usuarioPreferencia?.edit()?.putString(
+                                                    "username",
+                                                    binding.cogerUsuario.text.toString()
+                                                )?.apply()
+
+                                                val intent = Intent(this, MainActivity::class.java)
+                                                startActivity(intent)
+                                            } else {
+                                                Toast.makeText(
+                                                    this,
+                                                    "Authentication failed.",
+                                                    Toast.LENGTH_SHORT
+                                                ).show()
+                                            }
+                                        }
+                                } else {
+                                    //
+                                    Toast.makeText(
+                                        this,
+                                        "La cuenta que está intentando crear ya existe.\nTambién ha podido la ha puesto la contraseña mal.",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                }
+                            }
+                    }
+                }
         }
-
-
     }
-
-
-    fun saveFirestoreDataToJson(context: Context) {
-        val db = FirebaseFirestore.getInstance()
-
-        db.collection("recetas").get().addOnSuccessListener { querySnapshot ->
-            val articleList = mutableListOf<Map<String, Any>>()
-
-            for (document in querySnapshot) {
-                articleList.add(document.data)
-            }
-
-            val gson = GsonBuilder().setPrettyPrinting().create()
-            val json = gson.toJson(articleList)
-
-            val outputStream: OutputStream = context.openFileOutput("recetas.json", Context.MODE_PRIVATE)
-            outputStream.write(json.toByteArray())
-            outputStream.close()
-        }
-    }
-
 }
+
+
+fun saveFirestoreDataToJson(context: Context) {
+    val db = FirebaseFirestore.getInstance()
+
+    db.collection("recetas").get().addOnSuccessListener { querySnapshot ->
+        val articleList = mutableListOf<Map<String, Any>>()
+
+        for (document in querySnapshot) {
+            articleList.add(document.data)
+        }
+
+        val gson = GsonBuilder().setPrettyPrinting().create()
+        val json = gson.toJson(articleList)
+
+        val outputStream: OutputStream =
+            context.openFileOutput("recetas.json", Context.MODE_PRIVATE)
+        outputStream.write(json.toByteArray())
+        outputStream.close()
+    }
+}
+
